9faf3bf6423089a659cdae92e374e0a89afb51c2
Henrique Marques <lhenrique.dev@gmail.com>
2021-02-17 23:24:54 -0300

    scroll smooth nessa desgraça  :fire:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,17 @@ const tabs = document.querySelectorAll(".features__tab");
 const tabsContainer = document.querySelector(".features__tab-container");
 const tabsContent = document.querySelectorAll(".features__content");
 
+// Smooth scrolling
+document.querySelector(".nav__links").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href"); // #section--1
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Tabbed Content
 tabsContainer.addEventListener("click", (e) => {
   const clicked = e.target.closest(".features__tab");
 
@@ -9,14 +20,12 @@ tabsContainer.addEventListener("click", (e) => {
 
   // Remove active classes
   tabs.forEach((tab) => tab.classList.remove("features__tab--active"));
-
   tabsContent.forEach((content) =>
     content.classList.remove("features__content--active")
   );
 
   // Active tabs
   clicked.classList.add("features__tab--active");
-
   document
     .querySelector(`.features__content--${clicked.dataset.tab}`)
     .classList.add("features__content--active");
